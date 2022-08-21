2f35010747fdb767c2c40718f55e2b627b32e1c7
Bea <103050835+Beadko@users.noreply.github.com>
2022-08-21 20:35:04 +0700

    updated the ContactsForm file to be able to collect the contact information

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,11 +3,11 @@ import { useState, useEffect} from 'react';
 import { ContactForm } from '/Users/bea/Desktop/DevProject/react_challenge_start/src/components/contactForm/ContactForm.js';
 import { TileList } from '/Users/bea/Desktop/DevProject/react_challenge_start/src/components/tileList/TileList.js';
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = ({contacts, addContact}) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,21 +15,54 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if(!duplicate) {
+      addContact(name, phone, email);
+      setName("");
+      setPhone("");
+      setEmail("");
+    }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+  useEffect(() =>{
+    const IsDuplicate = () => {
+     const found = contacts.find((contact) => contact.name === name);
+      if (found !== undefined) {
+        return true;
+      }
+      return false;
+    };
+
+    if (IsDuplicate()) {
+      setDuplicate(true);
+    } else {
+      setDuplicate(false);
+    }
+  }, [name, contacts, duplicate]);
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          phone={phone}
+          setPhone={setPhone}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList
+        tiles={contacts}
+        />
       </section>
     </div>
   );
