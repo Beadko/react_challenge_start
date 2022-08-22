bce599f8194fc1ec47b4e72adb0e972753497f21
Bea <103050835+Beadko@users.noreply.github.com>
2022-08-22 10:29:18 +0700

    tileList updated. It is now able to render the list of Tile components

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Tile } from '/Users/bea/Desktop/DevProject/react_challenge_start/src/components/tile/Tile.js';
 
-export const TileList = () => {
+export const TileList = ({tiles}) => {
+
   return (
     <div>
-      
+      {tiles.map((tile, index) => {
+        <Tile key={index} tile={tile} />
+      })}
     </div>
   );
 };
