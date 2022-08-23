7e49f860baa57fba0e0187d28eefc7c60cc3c379
Bea <103050835+Beadko@users.noreply.github.com>
2022-08-23 20:49:32 +0700

    Added placeholders for the title input; removed label and onChange from the 'submit' input; added a label for the contactPicker component and added a '?' to the getContactNames() to check if array exists

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -20,7 +20,57 @@ export const AppointmentForm = ({
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
 
+  const getContactNames = () => {
+    return contacts?.map((contact) => contact.name);
+  };
+
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input 
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          placeholder="Appointment Title" 
+          type="text" 
+          name="title" 
+          value={title}
+        />
+      </label>
+      <br />
+      <label>
+        <input 
+          onChange={(e) => setDate(e.target.value)}
+          required
+          type="date" 
+          name="date" 
+          value={date}
+          min= {getTodayString()}
+        />
+      </label>
+      <br />
+      <label>
+        <input 
+          onChange={(e) => setTime(e.target.value)} 
+          type="time" 
+          name="time"
+          value={time}
+        />
+      </label>
+      <br />
+      <label>
+      <ContactPicker 
+        onChange={(e) => setContact(e.target.value)} 
+        contacts={getContactNames()}
+        name="contact"
+        value={contact}
+        placeholder="Appointment With" 
+      />
+      </label>
+      <br />
+        <input 
+          type="submit" 
+          value="Add Appointment"
+          />
+    </form>
   );
 };
