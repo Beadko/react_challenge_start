c42ea0ac21a86d60179f5b1014b53e342c4f3c42
Bea <103050835+Beadko@users.noreply.github.com>
2022-08-24 11:34:37 +0700

    fixed the type in the return, now the contacts are rendering

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Tile } from '/Users/bea/Desktop/DevProject/react_challenge_start/src/components/tile/Tile.js';
+import { Tile } from '../tile/Tile';
 
 export const TileList = ({tiles}) => {
 
   return (
     <div>
-      {tiles.map((tile, index) => {
+      {tiles.map((tile, index) => (
         <Tile key={index} tile={tile} />
-      })}
+        ))}
     </div>
   );
 };
